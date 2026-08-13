6a53ff728311afbc6980809f949d74b5eb3acf0f
shalini992006 <shalini992006@gmail.com>
2026-08-13 23:34:28 +0530

    Add files via upload

---
+++ Balanced Binary Tree.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def isBalanced(self, root):
+        def height(node):
+            if not node:
+                return 0
+            left = height(node.left)
+            right = height(node.right)
+            if abs(left - right) > 1:
+                return -1
+            if left == -1 or right == -1:
+                return -1
+            return 1 + max(left, right)
+        return height(root) != -1
+        
+        
+        
+
+        
+        
+        
+
+        
