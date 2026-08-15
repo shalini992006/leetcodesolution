6cfccd54b03d282490bb8ff8fd71f1fc1f0c6bf5
shalini992006 <shalini992006@gmail.com>
2026-08-15 22:09:01 +0530

    Add files via upload

---
+++ Valid Palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s):
+        clean = ""
+        for ch in s:
+            if ch.isalnum():
+                clean += ch.lower()
+        return clean == clean[::-1]
+        
+        
+
+
+
+        
