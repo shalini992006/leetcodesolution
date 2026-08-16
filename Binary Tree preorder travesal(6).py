4ceeb9c2925729831ec072e9ddd14913e97f8000
shalini992006 <shalini992006@gmail.com>
2026-08-16 23:01:39 +0530

    Add files via upload

---
+++ Binary Tree preorder travesal(6).py
@@ -0,0 +1,32 @@
+
+class Solution:
+    def preorderTraversal(self, root):
+        result = []
+        def preorder(node):
+            if node is None:
+                return
+            result.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+        preorder(root)
+        return result
+        
+            
+        
+        
+        
+    
+    
+
+        
+        
+
+
+        
+
+        
+        
+
+
+
+        
