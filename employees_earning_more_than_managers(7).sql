e08818e5ef6ef834e3f9e0742e1fa52b64cb6b32
shalini992006 <shalini992006@gmail.com>
2026-08-17 23:00:36 +0530

    Add files via upload

---
+++ employees_earning_more_than_managers(7).sql
@@ -0,0 +1,23 @@
+SELECT e.name AS Employee
+FROM employee e
+JOIN Employee m
+ON e.managerId = m.id
+WHERE e.salary > m.salary;
+        
+        
+        
+    
+    
+
+        
+        
+
+
+        
+
+        
+        
+
+
+
+        
